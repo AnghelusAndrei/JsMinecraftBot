ebc4d9c9fc75aae620ae5fadfed025901b0d53a2
yeetlol1 <rareschifu6@gmail.com>
2022-12-25 21:03:04 +0200

    heheheha

--- src/index.js
+++ src/index.js
@@ -4,41 +4,67 @@ const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathf
 const pvp = require('mineflayer-pvp').plugin
 
 class BotInstance{
-    static bot;
+    static bot
+
     constructor(bot){
-        this.bot = bot;
+        this.bot = bot
     }
-    view(viewer){
+    view(){
         bot.once('spawn', () => {
           mineflayerViewer(bot, { port: 3007, firstPerson: true })
         })
     }
-    hunt_single(user){
-        if(bot.players[user] === undefined){return}
+    travelDistance(user){
+        while(bot.players[user].entity.position.distanceTo(bot.entity.position) > bot.pvp.viewDistance){
+            var targetPosition = new Vec3()
+        }
+    }
+    huntSingle(instance, user){
+        if(bot.players[user] === undefined || user === bot.username){return false}
+
+        instance.travelDistance(user)
+
         const target = bot.players[user]
         bot.pvp.attack(target.entity)
+        bot.on('stoppedAttacking', () => {
+            bot.chat("heheheha") //heheheha
+            return true
+        })
     }
-    hunt_multiple(instance, users){
+    huntMultiple(instance, users){
+        var i = 0
+
         users.sort((a, b) => {
-            return bot.players[a].entity.position.distanceSquared(bot.entity.position) > bot.players[b].entity.position.distanceSquared(bot.entity.position)
+            if(bot.players[a] === undefined || bot.players[b] === undefined)return 1;
+            return bot.players[a].entity.position.distanceSquared(bot.entity.position) - bot.players[b].entity.position.distanceSquared(bot.entity.position)
         })
 
-        users.forEach(function (user, index) {
-            instance.hunt_single(user)
-        });
+        instance.huntSingle(users[0])
+
+        bot.on('stoppedAttacking', () => {
+            i++
+            while(!instance.huntSingle(users[i])){i++}
+        })
     }
 }
 const bot = mineflayer.createBot({
     host: 'localhost',
-    username: 'bot245',
-    port: 49347,
+    username: 'yeetlol',
+    port: 49258,
     version: "1.18"
 })
 
 const bot2 = mineflayer.createBot({
     host: 'localhost',
     username: 'bot246',
-    port: 49347,
+    port: 49258,
+    version: "1.18"
+})
+
+const bot3 = mineflayer.createBot({
+    host: 'localhost',
+    username: 'bot247',
+    port: 49258,
     version: "1.18"
 })
 
@@ -46,21 +72,21 @@ bot.loadPlugin(pathfinder)
 bot.loadPlugin(pvp)
 
 const instance = new BotInstance(bot)
-instance.view(mineflayerViewer)
+instance.view()
 
 
 bot.on('chat', (username, message) => {
   if (username === bot.username) return
-  const args = message.split(' ');                                  
+  var args = message.split(' ')                                  
   switch(args[0]){                                                  
     case "hunt":
         if(args.length > 1){
             args.splice(0,1)
-            instance.hunt_multiple(instance, args)
-            break;
+            instance.huntMultiple(instance, args)
+            break
         }
-        instance.hunt_single(username)
-        break;
+        instance.huntSingle(username)
+        break
     default:
         return;
   }
